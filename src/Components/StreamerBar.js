a0a4dbe9cb71db6a519a1c6e1aa44a3863b97e9b
jnzhang622 <jnzhang622@gmail.com>
2020-11-15 21:09:29 -0500

    setting up livecheck

--- src/Components/StreamerBar.js
+++ src/Components/StreamerBar.js
@@ -7,7 +7,19 @@ import StreamerBarItem from "./StreamerBarItem";
 
 function StreamerBar(props) {
     let reArrangedStreamers = props.streamers.sort((a,b) => a.name < b.name ? -1 : 1)
-
+    
+    liveCheck = (e) => {
+        e.preventDefault()
+        fetch(`https://api.twitch.tv/helix/search/channels?query=`, {
+            headers: {
+                'client-id': `${process.env.CLENTID}`,
+                Accept: 'application/json'
+            }
+        })
+        .then(resp => resp.json())
+        .then(data=>{this.props.handleNewPlaylist(data)}, this.setState({name: ""}))
+    }
+    
     return (
         <div>
             {
